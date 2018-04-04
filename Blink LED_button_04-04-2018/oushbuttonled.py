4f487ba9d2716ff6770e98e964fbf775f9721724
LUBT <triate@gmail.com>
2018-04-04 07:12:36 -0500

    Add files via upload
    Projeto de Blink do LED com um button, criação compartilhada.
    Ajuda do meu colega Fulvio Mascara

---
+++ Blink LED_button_04-04-2018/oushbuttonled.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#Definindo da biblioteca GPIO
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Aqui definimos que vamos usar o numero de ordem do pino, e não o numero que refere a porta
+#Para usar o numero da porta, é preciso trocar a definição "GPIO.BOARD (ex. Pino 12)" para "GPIO.BCM (ex.GPIO 18)" 
+GPIO.setmode(GPIO.BOARD)
+
+# Setando as portas de entrada e saída
+ledPin = 7
+buttonPin = 11
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.output(ledPin, False)
+
+#Loop principal (Laço indefinido)
+try:
+    while(True):
+        GPIO.output(ledPin, not GPIO.input(buttonPin))
+        sleep(.1)
+finally:
+    GPIO.output(ledPin, False)
+    GPIO.cleanup()
