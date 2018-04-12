5938ddcd981fc51f251582c460ea3b4c4842dc85
LUBT <triate@gmail.com>
2018-04-12 09:39:54 -0500

    Add files via upload

---
+++ Raspberry-pi-sensor-umidade-temperatura-DHT11_06-04-2018/pi3-and-dht11.py
@@ -0,0 +1,28 @@
+# Programa : Sensor de temperatura DHT11 com Raspberry Pi B+
+# Autor : Lucas de Barros
+ 
+# Carrega as bibliotecas
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+import requests
+import json
+ 
+# Define o tipo de sensor
+sensor = Adafruit_DHT.DHT11
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+# Define a GPIO conectada ao pino de dados do sensor,
+pino_sensor = 25
+ 
+# Informacoes iniciais
+ 
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pino_sensor)
+    data = {'Umidade':humidity,'Temperatura':temperature}
+    #json_data = json.dumps(data)
+    #r = requests.post('http://192.168.11.112:1880/payload',json_data)
+    print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+    time.sleep(2)
